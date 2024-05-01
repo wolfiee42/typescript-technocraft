a4c2673948b0f8dfc64203cda9775cc183a7dd28
wolfiee42 <saifalislam2022@gmail.com>
2024-05-01 21:21:30 +0600

    understanding along with practicing oremative and non-premative data types declarations in typescript

---
+++ module-1/src/1.4.ts
@@ -0,0 +1,25 @@
+// number 
+let age: number = 23;
+
+// string
+let friendsName: string = "saif";
+
+// boolean
+let isMarried: boolean = false;
+
+// null
+let companyName: null = null;
+
+//undefined
+let whereIsTheKey: undefined = undefined;
+
+// Array
+let friendsNamesAre: string[] = ['ashik', 'ali', 'nahin', 'rayan', 'ashraf'];;
+
+// tuple // which represents array, where array length is fixed so does types
+let matchAndScores: [string, number, number] = ['rmaVSbayern', 2, 2]
+
+let a: string;
+a = 'wer';
+// a = 123;  error
+// a = true error
